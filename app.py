fc99443d11dae11f28213c927abe79c0c223baa0
Ian Penny <ipenny@umd.edu>
2023-03-28 19:35:10 -0400

    Added pretty much all my code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -12,6 +12,24 @@ def hello():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/favorite_course')
+def favorite_course():
+    print("You entered your favorite course as: " + request.args.get('subject'))
+    print("You entered your favorite course as: " + request.args.get('course_number'))
+
+    return render_template('favorite_course.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run()
